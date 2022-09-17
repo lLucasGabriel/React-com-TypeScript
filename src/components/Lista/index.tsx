922ccd109ea182abbdf680b5f739e4cede748054
Lucas Gabriel <llucas182011@hotmail.com>
2022-09-17 09:48:25 -0300

    initial commit

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState } from 'react';
+import Item from "./Item";
+import style from './Lista.module.scss'
+
+function Lista() {
+
+    const [tarefas, setTarefas] = useState(
+        [{
+            tarefa: 'React',
+            tempo: '02:00:00'
+        }, {
+            tarefa: 'JavaScript',
+            tempo: '01:00:00'
+        }, {
+            tarefa: "TypeScript",
+            tempo: "03:00:00"
+        }]
+    )
+
+    return(
+        <aside className={style.listaTarefas}>
+            <h2 onClick={() => (
+                setTarefas([...tarefas, {tarefa: "Estudar estado", tempo: "05:00:00"}])
+            )}>Estudos do dia</h2>
+            <ul>       
+                {tarefas.map((item, index) => (
+                <Item
+                    key={index}
+                    tarefa={item.tarefa}
+                    tempo={item.tempo}
+                />
+                ))}
+            </ul>
+        </aside>
+    )
+}
+
+export default Lista
---
+++ src/components/pages/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Cronometro from '../Cronometro';
+import Formulario from '../Formulario';
+import Lista from '../Lista';
+import style from './App.module.scss'
+
+function App() {
+  return (
+    <div className={style.AppStyle}>   
+      <Formulario/>
+      <Lista/>
+      <Cronometro/>
+    </div>
+  );
+}
+
+export default App;
